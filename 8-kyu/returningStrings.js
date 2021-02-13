fe1fd207605ebe75fd88fef874b15dc6eed3dc01
Aaron Thomas <deadbeginning@gmail.com>
2021-02-13 20:02:44 +0000

    new challenge

---
+++ 8-kyu/returningStrings.js
@@ -0,0 +1,7 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/solutions/javascript
